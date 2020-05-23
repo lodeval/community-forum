092165ac9422732bc5331999ff536b6de97c86dd
Laurent Delluc <laurent_delluc@yahoo.fr>
2020-05-23 17:45:25 +0200

    initial commit

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from '@vue/test-utils';
+import Welcome from '@/components/Welcome.vue';
+
+describe('Welcome.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(Welcome, {
+      propsData: { msg },
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
